90f4390b0f3171c2b9a8a64ec1a8a17185d395b8
synycboom <synycboom@gmail.com>
2021-12-16 17:18:22 +0700

    feat: initialize package

---
+++ packages/vue-follow-button/babel.config.js
@@ -0,0 +1,15 @@
+const devPresets = ['@vue/babel-preset-app'];
+const buildPresets = [
+  [
+    '@babel/preset-env',
+    {
+      include: [
+        /(optional-chaining|nullish-coalescing)/
+      ],
+    },
+  ],
+  '@babel/preset-typescript',
+];
+module.exports = {
+  presets: (process.env.NODE_ENV === 'development' ? devPresets : buildPresets),
+};
---
+++ packages/vue-follow-button/dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueFollowButton from '@/vue-follow-button.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueFollowButton
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-follow-button />
+  </div>
+</template>

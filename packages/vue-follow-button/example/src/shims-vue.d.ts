6741925eb902499fa285242022f3c095e070bcbf
synycboom <synycboom@gmail.com>
2021-12-16 18:43:17 +0700

    feat: add example

---
+++ packages/vue-follow-button/example/src/shims-vue.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+interface Window {
+  ethereum?: {
+    isMetaMask?: true;
+    request?: (...args: any[]) => Promise<Object[]>;
+  };
+}

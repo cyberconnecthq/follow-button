0c7002f3a3a648ab9591c528d250f6861687f474
Jiayi Xin <jiayi920429@gmail.com>
2022-04-01 15:01:48 -0700

    chore: update react-follow-button version

--- packages/react-follow-button/demo/src/App.tsx
+++ packages/react-follow-button/demo/src/App.tsx
@@ -1,22 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import Web3 from 'web3';
+import { useState } from "react";
+import "./App.css";
+import Web3 from "web3";
 import {
   FollowButton,
   Env,
   Blockchain,
-} from '@cyberconnect/react-follow-button';
+} from "@cyberconnect/react-follow-button";
 
 function App() {
-  const [account, setAccount] = useState<string>('');
+  const targetAddr = "0xe6aab1f16ff560d309ed7ce8e08d290306a0906c";
+  const [account, setAccount] = useState<string>("");
   const [isWalletConnected, setIsWalletConnected] = useState<boolean>(false);
 
   const connectWallet = async () => {
-    if (typeof window.ethereum === 'undefined') {
-      console.error('MetaMask is installed!');
+    if (typeof window.ethereum === "undefined") {
+      console.error("MetaMask is installed!");
     }
     const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
+      method: "eth_requestAccounts",
     });
 
     setAccount(accounts[0]);
@@ -30,11 +31,13 @@ function App() {
           <div className="address">
             <b>Account:</b> {account}
           </div>
+          <div className="address">
+            <b>Target:</b> {targetAddr}
+          </div>
           <FollowButton
-            key="0xe6aab1f16ff560d309ed7ce8e08d290306a0906c"
             provider={Web3.givenProvider}
             namespace="CyberConnect"
-            toAddr="0xe6aab1f16ff560d309ed7ce8e08d290306a0906c"
+            toAddr={targetAddr}
             env={Env.PRODUCTION}
             chain={Blockchain.ETH}
             onSuccess={(e) => {
